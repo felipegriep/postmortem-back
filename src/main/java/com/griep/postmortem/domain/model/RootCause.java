a70cea451436eb61a2c151785c182e0cfd25e0a9
Felipe Griep <griep.felipe@gmail.com>
2025-11-03 00:45:15 -0300

    Ajustada a entidade RootCause.

--- src/main/java/com/griep/postmortem/domain/model/RootCause.java
+++ src/main/java/com/griep/postmortem/domain/model/RootCause.java
@@ -28,25 +28,25 @@ public class RootCause {
                 foreignKey = @ForeignKey(name = "FK_ROOT_CAUSE_ID_INCIDENT"))
     private Incident incident;
 
-    @Column(name = "WHY1", nullable = false, length = 4000)
+    @Column(name = "WHY1", nullable = false, length = 500)
     private String why1;
 
-    @Column(name = "WHY2", nullable = false, length = 4000)
+    @Column(name = "WHY2", nullable = false, length = 500)
     private String why2;
 
-    @Column(name = "WHY3", nullable = false, length = 4000)
+    @Column(name = "WHY3", nullable = false, length = 500)
     private String why3;
 
-    @Column(name = "WHY4", nullable = false, length = 4000)
+    @Column(name = "WHY4", nullable = false, length = 500)
     private String why4;
 
-    @Column(name = "WHY5", nullable = false, length = 4000)
+    @Column(name = "WHY5", nullable = false, length = 500)
     private String why5;
 
-    @Column(name = "ROOT_CAUSE_TEXT", nullable = false, length = 4000)
+    @Column(name = "ROOT_CAUSE_TEXT", nullable = false, length = 800)
     private String rootCauseText;
 
-    @Column(name = "CONTRIBUTING_FACTORS", length = 4000)
+    @Column(name = "CONTRIBUTING_FACTORS", length = 800)
     private String contributingFactors;
 
     @Lob
